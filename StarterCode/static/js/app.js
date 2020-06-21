3b57a6b42c8dc2ac96a7cc7baf50b440b123e02e
Terrence Cummings <terrence.cummings@gmail.com>
2020-06-21 17:18:13 -0500

    bar chart and demog table working

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,15 +1,18 @@
 // Plotly Homework
 // Terrence Cummings
 
-
+// Get the JSON data
 d3.json("samples.json").then((data) => {
+    // Prepare to merge the Samples Data with the MetaData
+    // Ensure the patient ID in the Sample Data and MetaData line up via sort
     let sortedSamples = data.samples.sort((a, b) => parseInt(a.id) - parseInt(b.id));
     let sortedMetaData = data.metadata.sort((a, b) => a.id - b.id);
-    // sortedMetaData.forEach(addSamples);
 
+    // Add an array of objects regarding each sample for each patient to the patient Metadata
     for (let i = 0; i < sortedMetaData.length; i++) {
         sortedMetaData[i].testData = [];
         for (let j = 0; j < sortedSamples[i].otu_ids.length; j++) {
+            // Create an object for each sample containing ID, label, and value
             sortedMetaData[i].testData[j] = {
                 'otuID': sortedSamples[i].otu_ids[j],
                 'otuLabel': sortedSamples[i].otu_labels[j],
@@ -19,40 +22,85 @@ d3.json("samples.json").then((data) => {
     };
     console.log(sortedMetaData);
 
-    let patientZero = sortedMetaData[0];
-    console.log(patientZero);
-    let sortedTestData = patientZero.testData.sort((a, b) => b.sampleValue - a.sampleValue);
-    console.log(sortedTestData);
-    let topTenTestData = sortedTestData.slice(0, 10).reverse();
-    console.log(topTenTestData);
+    // Listen for dropdown menu selecting the patient number to chart
+    d3.select('#selDataset').on('change', barChart);
 
-    // Trace1 for the Greek Data
-    let trace1 = {
-        x: topTenTestData.map(object => object.sampleValue),
-        y: topTenTestData.map(object => "OTU " + object.otuID),
-        text: topTenTestData.map(object => object.otuLabel),
-        name: "Belly Button",
-        type: "bar",
-        orientation: "h"
+    // Update the bar chart and the demographic table with the data of the patient selected
+    function barChart() {
+        // Get the ID of the individual from the dropdown menu value selected
+        let inputID = d3.select('#selDataset').node().value;
+
+        // Find the array index associated with the individual selected
+        let index = sortedMetaData.findIndex(p => p.id == inputID);
+        console.log(index);
+
+
+        // Get the demographic data for the individual to display in the table
+        let patientZero = sortedMetaData[index];
+        let patientID = patientZero.id;
+        let patientEthnic = patientZero.ethnicity;
+        let patientGender = patientZero.gender;
+        let patientAge = patientZero.age;
+        let patientLocation = patientZero.location;
+        let patientBbtype = patientZero.bbtype;
+        let patientWfreq = patientZero.wfreq;
+        let patientNumSamp = patientZero.testData.length;
+
+        console.log(patientID);
+        console.log(patientEthnic);
+        console.log(patientGender);
+        console.log(patientAge);
+        console.log(patientLocation);
+        console.log(patientBbtype);
+        console.log(patientWfreq);
+        console.log(patientNumSamp);
+
+        // Update the demographic table with the individual's data
+        d3.select('#patientID').text('id: ' + patientID);
+        d3.select('#ethnicity').text('ethnicity: ' + patientEthnic);
+        d3.select('#gender').text('gender: ' + patientGender);
+        d3.select('#age').text('age: ' + patientAge);
+        d3.select('#location').text('location: ' + patientLocation);
+        d3.select('#bbtype').text('bbtype: ' + patientBbtype);
+        d3.select('#wfreq').text('wfreq: ' + patientWfreq);
+        d3.select('#numsamp').text('samples: ' + patientNumSamp);
+
+        // Sort the sample data of the individual based on descending sample values
+        let sortedTestData = patientZero.testData.sort((a, b) => b.sampleValue - a.sampleValue);
+        console.log(sortedTestData);
+
+        // Take the top 10 largest sample values. Reverse the order for the horizontal bar chart
+        let topTenTestData = sortedTestData.slice(0, 10).reverse();
+        console.log(topTenTestData);
+
+        // Set the trace info for the horizontal bar chart
+        let trace1 = {
+            x: topTenTestData.map(object => object.sampleValue),
+            y: topTenTestData.map(object => "OTU " + object.otuID),
+            text: topTenTestData.map(object => object.otuLabel),
+            name: "Belly Button",
+            type: "bar",
+            orientation: "h"
+        };
+
+        // Convert trace to data
+        let data1 = [trace1];
+
+        // Bar chart layout
+        let layout = {
+            title: "Belly Button Bacteria Volunteer " + sortedMetaData[index].id,
+            margin: {
+                l: 100,
+                r: 100,
+                t: 100,
+                b: 100
+            }
+        };
+
+        // Plot the horizontal bar chart for the selected individual
+        Plotly.newPlot("bar", data1, layout);
+
     };
-
-    // data
-    let data1 = [trace1];
-
-    // Apply the group bar mode to the layout
-    let layout = {
-        title: "Belly Button Bacteria Volunteer " + sortedMetaData[0].id,
-        margin: {
-            l: 100,
-            r: 100,
-            t: 100,
-            b: 100
-        }
-    };
-
-    // Render the plot to the div tag with id "plot"
-    Plotly.newPlot("bar", data1, layout);
-
 
 
 
