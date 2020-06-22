19b1f4da6727860eaeb17f51c9905b43e0514e6e
Terrence Cummings <terrence.cummings@gmail.com>
2020-06-22 10:39:50 -0500

    all charts and gauges working

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -88,7 +88,7 @@ d3.json("samples.json").then((data) => {
 
         // Bar chart layout
         let layout1 = {
-            title: "Belly Button Bacteria: Subject ID " + sortedMetaData[index].id,
+            title: "Belly Button Bacteria",
             // margin: {
             //     l: 100,
             //     r: 100,
@@ -100,8 +100,8 @@ d3.json("samples.json").then((data) => {
         // Plot the horizontal bar chart for the selected individual
         Plotly.newPlot("bar", data1, layout1);
 
-
-        var trace2 = {
+        // Start bubble chart
+        let trace2 = {
             x: sortedTestData.map(object => object.otuID),
             y: sortedTestData.map(object => object.sampleValue),
             text: sortedTestData.map(object => object.otuLabel),
@@ -113,16 +113,72 @@ d3.json("samples.json").then((data) => {
             }
         };
 
-        var data2 = [trace2];
+        let data2 = [trace2];
 
-        var layout2 = {
-            title: 'Bubble Chart Hover Text',
+        let layout2 = {
+            title: 'Belly Button Bacteria',
             showlegend: false,
             // height: 600,
             // width: 600
         };
 
         Plotly.newPlot('bubble', data2, layout2);
+
+        // Start gauge chart
+
+        let traceA = {
+            type: "pie",
+            showlegend: false,
+            hole: 0.4,
+            rotation: 90,
+            values: [100 / 9, 100 / 9, 100 / 9, 100 / 9, 100 / 9, 100 / 9, 100 / 9, 100 / 9, 100 / 9, 100],
+            text: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", ""],
+            direction: "clockwise",
+            textinfo: "text",
+            textposition: "inside",
+            marker: {
+                colors: [
+                    'rgb(31, 119, 180)', 'rgb(255, 127, 14)',
+                    'rgb(44, 160, 44)', 'rgb(214, 39, 40)',
+                    'rgb(148, 103, 189)', 'rgb(140, 86, 75)',
+                    'rgb(227, 119, 194)', 'rgb(127, 127, 127)',
+                    'rgb(188, 189, 34)', "white"
+                ],
+                colorscale: 'Earth'
+            },
+            // labels: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", ""],
+            hoverinfo: ""
+        };
+
+        let degrees = patientWfreq / 9 * 180,
+            radius = .5;
+        let radians = (180 - degrees) * Math.PI / 180;
+        let x = radius * Math.cos(radians) + 0.5;
+        let y = radius * Math.sin(radians) + 0.5;
+
+        let layoutA = {
+            shapes: [{
+                type: 'line',
+                x0: 0.5,
+                y0: 0.5,
+                x1: x,
+                y1: y,
+                line: {
+                    color: 'black',
+                    width: 8
+                }
+            }],
+            title: 'Belly Button Washing Frequency',
+            xaxis: { visible: false, range: [-1, 1] },
+            yaxis: { visible: false, range: [-1, 1] }
+        };
+
+        let dataA = [traceA];
+
+        Plotly.newPlot("gauge", dataA, layoutA, { staticPlot: false });
+
+
+
 
     };
 
@@ -140,100 +196,3 @@ d3.json("samples.json").then((data) => {
 
 
 });
-
-// DAY 3, EXERCISE 5 EXAMPLE BELOW HERE
-// function unpack(rows, index) {
-//     return rows.map(function(row) {
-//         return row[index];
-//     });
-// }
-
-// function getMonthlyData() {
-
-//     var queryUrl = `https://www.quandl.com/api/v3/datasets/WIKI/AMZN.json?start_date=2016-10-01&end_date=2017-10-01&collapse=monthly&api_key=${apiKey}`;
-//     d3.json(queryUrl).then(function(data) {
-//         var dates = unpack(data.dataset.data, 0);
-//         var openPrices = unpack(data.dataset.data, 1);
-//         var highPrices = unpack(data.dataset.data, 2);
-//         var lowPrices = unpack(data.dataset.data, 3);
-//         var closingPrices = unpack(data.dataset.data, 4);
-//         var volume = unpack(data.dataset.data, 5);
-//         buildTable(dates, openPrices, highPrices, lowPrices, closingPrices, volume);
-//     });
-// }
-
-// function buildTable(dates, openPrices, highPrices, lowPrices, closingPrices, volume) {
-//     var table = d3.select("#summary-table");
-//     var tbody = table.select("tbody");
-//     var trow;
-//     for (var i = 0; i < 12; i++) {
-//         trow = tbody.append("tr");
-//         trow.append("td").text(dates[i]);
-//         trow.append("td").text(openPrices[i]);
-//         trow.append("td").text(highPrices[i]);
-//         trow.append("td").text(lowPrices[i]);
-//         trow.append("td").text(closingPrices[i]);
-//         trow.append("td").text(volume[i]);
-//     }
-// }
-
-// function buildPlot() {
-//     var url = `https://www.quandl.com/api/v3/datasets/WIKI/AMZN.json?start_date=2017-01-01&end_date=2018-11-22&api_key=${apiKey}`;
-
-//     d3.json(url).then(function(data) {
-
-//         // Grab values from the response json object to build the plots
-//         var name = data.dataset.name;
-//         var stock = data.dataset.dataset_code;
-//         var startDate = data.dataset.start_date;
-//         var endDate = data.dataset.end_date;
-//         var dates = unpack(data.dataset.data, 0);
-//         var openingPrices = unpack(data.dataset.data, 1);
-//         var highPrices = unpack(data.dataset.data, 2);
-//         var lowPrices = unpack(data.dataset.data, 3);
-//         var closingPrices = unpack(data.dataset.data, 4);
-
-//         getMonthlyData();
-
-//         var trace1 = {
-//             type: "scatter",
-//             mode: "lines",
-//             name: name,
-//             x: dates,
-//             y: closingPrices,
-//             line: {
-//                 color: "#17BECF"
-//             }
-//         };
-
-//         // Candlestick Trace
-//         var trace2 = {
-//             type: "candlestick",
-//             x: dates,
-//             high: highPrices,
-//             low: lowPrices,
-//             open: openingPrices,
-//             close: closingPrices
-//         };
-
-//         var data = [trace1, trace2];
-
-//         var layout = {
-//             title: `${stock} closing prices`,
-//             xaxis: {
-//                 range: [startDate, endDate],
-//                 type: "date"
-//             },
-//             yaxis: {
-//                 autorange: true,
-//                 type: "linear"
-//             },
-//             showlegend: false
-//         };
-
-//         Plotly.newPlot("plot", data, layout, { responsive: false });
-
-//     });
-// }
-
-// buildPlot();
